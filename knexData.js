3103ba8b1d2ebdf8e0a55141bd2686f1320d76ff
Phuong <1760393@student.hcmus.edu.vn>
2019-11-12 14:18:02 +0700

    fix bug

--- knexData.js
+++ knexData.js
@@ -1,12 +1,13 @@
 var knex = require("knex")({
-    client: "pg",
-    connection: {
-      host: process.env.Host,
-      user: process.env.User,
-      password: process.env.Password,
-      database: process.env.Database,
-      ssl: true
-    }
-  });
+  client: "pg",
+  connection: {
+    host: "ec2-54-83-9-169.compute-1.amazonaws.com",
+    user: "vkrpsybninavbh",
+    password:
+      "bf07a712499c29f09cf7d411b8c60c3d694a7a2f03119849f98184a27d599ac3",
+    database: "dd1tqg3tu3ugco",
+    ssl: true
+  }
+});
 
-exports.default=knex
+exports.default = knex;
