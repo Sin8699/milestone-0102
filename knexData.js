83be56856700aea454ab0a65856226dbf2bc8236
Phuong <1760393@student.hcmus.edu.vn>
2019-11-29 18:06:41 +0700

    add transaction

--- knexData.js
+++ knexData.js
@@ -1,14 +1,6 @@
 var knex = require("knex")({
   client: "pg",
-  connection: process.env.DATABASE_URL + `?ssl=true`,
-  // connection: {
-  //   host: "ec2-174-129-33-167.compute-1.amazonaws.com",
-  //   user: "ftbskghtdjinwq",
-  //   password:
-  //     "fe0f5f08407cd2aa2149a7c8b1dcda42efe3a37ecf011758d585e0e835a6354b",
-  //   database: "d3ju9q61kpb0r8",
-  //   ssl: true
-  // }
+  connection: process.env.DATABASE_URL + `?ssl=true`
 });
 
 exports.default = knex;
